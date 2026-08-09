4759626c01e63ee3a24e2967a3d532ed7cb20b26
Arien Tschemeris <atschemeris@icloud.com>
2026-08-09 18:49:58 +0200

    Add comment about Google Analytics initialization

--- artifacts/web-app/src/main.tsx
+++ artifacts/web-app/src/main.tsx
@@ -2,4 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Google Analytics is initialized in index.html
+// gtag will be available as window.gtag
+
 createRoot(document.getElementById("root")!).render(<App />);
